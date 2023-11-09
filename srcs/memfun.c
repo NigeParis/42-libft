ae1c0fab620296c1842a2252bd64474b297d8885
Nigel Robinson <nrobinso@paul-f4Ar8s6.clusters.42paris.fr>
2023-11-09 17:10:50 +0100

    correcteur installed

---
+++ srcs/memfun.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrobinso <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 15:41:12 by nrobinso          #+#    #+#             */
+/*   Updated: 2023/11/09 08:02:16 by nrobinso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/**
+* #include <string.h>
+*
+* memmove  -  void *memmove(void *dest, const void *src, size_t n);
+*
+* DESCRIPTION
+* The memmove() fun copies n bytes from memory area src to memory area dest.  
+* The memory areas may overlap: copying takes place as though the bytes in src
+* are first copied into a temporary array that does not overlap src or dest, 
+* and the bytes are then copied from the temporary array to dest.
+*
+* EXAMPLE   char a[30] = "That's not what I said." - memmove( a+7, a+11, 13 ) 
+*						before : That's not what I said.$
+*						after  : That's what I said.$
+**/
+/*
+#include "libft.h"
+
+void	*memmove(void *dest, const	void *src, size_t n)
+{
+
+
+
+
+
+
+}
+
+*/
--- srcs/test.c
+++ srcs/test.c
@@ -6,19 +6,59 @@
 /*   By: nrobinso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 07:59:18 by nrobinso          #+#    #+#             */
-/*   Updated: 2023/11/09 08:01:35 by nrobinso         ###   ########.fr       */
+/*   Updated: 2023/11/09 16:56:27 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
 #include <stdio.h>
 #include "libft.h"
+#include <string.h>
+
+void	*ftmemmove(void *dest, const void *src, size_t n)
+{
+	unsigned char *ptr_dest;
+	unsigned char *ptr_src;
+	size_t position;
+
+	ptr_dest = (unsigned char *)dest;
+	ptr_src = (unsigned char *)src;
+	position = n;
+
+	while (position)
+	{
+		if (1)
+		{
+			ptr_dest[position] = ptr_src[position]; // dst == buffer
+			printf("1 -");
+		}
+		else
+		{	
+			ptr_dest [position - 1] = ptr_src [position - 1]; // dst != buffer
+			printf("2 -");
+
+		}
+		position--;
+	}
+
+		printf("dest %s  ", ptr_dest);
+return (&ptr_dest[0]);
+}
+
 
 int main(void)
 {
+char carr_dest[] = "134";
+char word_dest[] = "134";
+char word_src[] = "AbCfed";
+char carr_src[] = "AbCfed";
 
 
-	printf("Hello");
+	ftmemmove(word_dest, word_src, 4);
+	memmove(carr_dest, carr_src, 4);
 
 
+	printf("carr_F memory : %s\n", word_dest);
+	printf("           -word_V memory : %s\n",carr_dest);
 
-	return (0);
-}
+return (0);
+}*/
