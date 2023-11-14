e9526d50a577ed222f21caaa253f91b07856d960
nige <nige@MacBook-Pro-de-nige.local>
2023-11-14 16:30:59 +0100

    started ft_memccpy

---
+++ srcs/ft_memccpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 16:04:24 by nrobinso          #+#    #+#             */
+/*   Updated: 2023/11/14 16:24:05 by nrobinso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/**
+* #include <string.h>
+*
+* memccpy  - 
+*
+* void *
+*    memccpy(void *restrict dst, const void *restrict src, int c, size_t n);
+*
+* DESCRIPTION
+* The memccpy() function copies bytes from string src to string dst. If
+* the character c (as converted to an unsigned char) occurs in the string
+* src, the copy stops and a pointer to the byte after the copy of c in the
+* string dst is returned.  
+* Otherwise, n bytes are copied, and a NULL pointer is returned.
+*
+* The source and destination strings should not overlap, as the behavior 
+* is undefined.
+**/
+
+
+ 
+
