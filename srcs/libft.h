920ea24e421addcf569c02a612e7aacf76565828
nige <nige@MacBook-Pro-de-nige.local>
2023-11-19 21:43:23 +0100

    ft_itoa finnished

--- srcs/libft.h
+++ srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: nrobinso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 15:19:06 by nrobinso          #+#    #+#             */
-/*   Updated: 2023/11/16 14:36:48 by nrobinso         ###   ########.fr       */
+/*   Updated: 2023/11/19 17:30:29 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,4 +55,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+//void	ft_itoa(int n);
+char	*ft_itoa(int n);
 #endif
