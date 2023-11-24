e251f944f4c36b6e3c4957b10cc26139c5c84735
Nigel Robinson <nrobinso@paul-f4Ar8s6.clusters.42paris.fr>
2023-11-24 09:28:39 +0100

    working at the school

--- libft/bonus_main.c
+++ libft/bonus_main.c
@@ -6,7 +6,7 @@
 /*   By: nige42 <Nige@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 09:11:41 by nige42            #+#    #+#             */
-/*   Updated: 2023/11/23 09:15:18 by nige42           ###   ########.fr       */
+/*   Updated: 2023/11/24 07:47:47 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,28 @@
 
 int	main(void)
 {
+	t_list	*ptr;
+	t_list	*new;
+	char str[] = "Hello";
+	char str1[] = "World";
 
-ft_test();
+	ptr = ft_lstnew(str);
+	new = ft_lstnew(str1);
 
+/*	printf("\ncontenu : %s", (char *)ptr->content);
+	printf("\nnext... : %p", (char *)ptr->next);
+	printf("\ncontenu : %s", (char *)new->content);
+	printf("\nnext... : %p", (char *)new->next);
+*/	
+	ft_lstadd_front(&ptr, new);
 
-
+	t_list *tmp = ptr;
+	printf("\nprinting the full list...\n");
+	while (tmp)
+	{
+		printf("\ncontenu : %s", (char *)tmp->content);
+		printf("\nnext... : %p", tmp->next);
+		tmp = tmp->next;
+	}
 	return (0);
 }
