a1e47feb92206ca3a50778a7db1c3a054c8e1b50
Nigel Robinson <nrobinso@made-f0Cr5s1.clusters.42paris.fr>
2023-11-28 14:05:08 +0100

    minor error with split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 13:55:07 by nrobinso          #+#    #+#             */
-/*   Updated: 2023/11/22 17:57:31 by nrobinso         ###   ########.fr       */
+/*   Updated: 2023/11/28 14:04:49 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /**
@@ -62,7 +62,7 @@ char	**ft_spliter(char const *s, char c, size_t next, size_t i)
 		if ((ft_nb_chars(&s[i], c)) > 0)
 		{
 			str[next] = ft_alloc_free(str, ((ft_nb_chars(&s[i], c))), next);
-			if (!str)
+			if (!str[next])
 				return (NULL);
 			ft_memcpy(str[next], &s[i], (ft_nb_chars(&s[i], c)));
 			i = i + (ft_nb_chars(&s[i], c));
